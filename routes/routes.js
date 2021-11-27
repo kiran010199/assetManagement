b93e6020abfa81b6a61cb72d434b4ed8ffd38154
kiran010199 <kiran.giby@experionglobal.com>
2021-11-27 16:46:33 +0530

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const assetRoutes = require('./assets.route');
+const userRoutes = require('./users.route');
 
 router.use('/asset', assetRoutes);
+module.exports = router;
+
+router.use('/user', userRoutes);
 module.exports = router;
